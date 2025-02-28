657f35d8cb106c8ae76c33d207b5fea701f9c592
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-28 12:39:56 +0100

    feat: Removed new lines

--- src/scripts/text-to-catalog/merge-pages.ts
+++ src/scripts/text-to-catalog/merge-pages.ts
@@ -1,7 +1,8 @@
 const mergePages = (text: string): string => {
   return text
     .replace(/\n\n\d+\nДержавний архів.+$/gm, "")
-    .replace(/\n\n\d+\nМіжархівний довідник/gm, "");
+    .replace(/\n\n\d+\nМіжархівний довідник/gm, "")
+    .replace(/\n((?!\d{1,2}\.).+)/g, "$1");
 };
 
 export default mergePages;
