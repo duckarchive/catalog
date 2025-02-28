f98faf09159535122dd58dfa06d1c598c7bfc85f
alexandrtovmach <alexandrtovmach@gmail.com>
2025-02-28 12:41:52 +0100

    fix: Removed doubled new lines

--- src/scripts/text-to-catalog/merge-pages.ts
+++ src/scripts/text-to-catalog/merge-pages.ts
@@ -2,7 +2,8 @@ const mergePages = (text: string): string => {
   return text
     .replace(/\n\n\d+\nДержавний архів.+$/gm, "")
     .replace(/\n\n\d+\nМіжархівний довідник/gm, "")
-    .replace(/\n((?!\d{1,2}\.).+)/g, "$1");
+    .replace(/\n((?!\d{1,2}\.).+)/g, "$1")
+    .replace(/\n\n/g, "\n")
 };
 
 export default mergePages;
