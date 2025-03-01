eae870f2ed30d9bdff53824fae6813f263f66843
alexandrtovmach <alexandrtovmach@gmail.com>
2025-03-01 14:56:25 +0100

    fix: Cut off unused text

--- src/scripts/text-to-catalog/cleanup.ts
+++ src/scripts/text-to-catalog/cleanup.ts
@@ -2,6 +2,7 @@ const cleanup = (text: string): string => {
   return text
     .replace(/\n\n\d+\nДержавний архів.+$/gm, "")
     .replace(/\n\n\d+\nМіжархівний довідник/gm, "")
+    .replace(/покажчик\sнаселених\sпунктів(.|\n)+/igm, "")
     .replace(/^\s+(\d{1,2}\.)/gm, "$1")
     .replace(/\n((?!\d{1,2}\.).+)/g, "$1")
     .replace(/^(\s+)?\n/gm, "")
